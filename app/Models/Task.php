0703abe5a51efaba52a5cfad81035d4cf76f5fef
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2025-07-29 08:00:13 +0100

    relacionamento entre tabelas finalizado

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'priority',
+        'id_user',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'image',
         'password',
     ];
 
@@ -44,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
     }
 }
