867c2906c259302bfa13e9d17300e14e941cfa81
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2025-07-29 08:28:01 +0100

    criando o registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,17 @@
 @extends('auth.layout')
-@section('title', 'Login')
+@section('title', 'Cadastro')
+
 
 @section('content')
 
     <div class="container mt-4">
 
-        <h2>Bem-vindo de volta!</h2>
+        <h2>Sejá bem vindo!</h2>
+        <p>Cadastre - se e gerencie suas tarefas agora.</p>
 
         <br>
 
-        <form method="POST" action="">
-            @csrf
+        <form>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -24,9 +25,9 @@
                 <label class="form-check-label" for="exampleCheck1">Lembrar-me</label>
             </div>
             <div class="mb-3">
-                Ainda não tem uma conta? <a href="{{ route('user.index') }}">Criar</a>
+                Já possui uma conta? <a href="{{ route('auth.index') }}">Fazer login</a>
             </div>
-            <button type="submit" class="btn btn-primary">Login</button>
+            <button type="submit" class="btn btn-primary">Criar</button>
         </form>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -9,3 +10,5 @@ use Illuminate\Support\Facades\Route;
  * Auth
  */
 Route::get('/login', [AuthController::class, 'index'])->name('auth.index');
+
+Route::get('/register', [UserController::class, 'index'])->name('user.index');
